2001cc16ce02ed56fc80812dadcbf8984d672ee4
HBA114 <hbasriayhaner114@gmail.com>
2023-07-03 18:03:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// Valid Parentheses Question from a Junior Developer Interview
+
+using System.Text;
+
+var res = GeneratePattern(10);
+Console.WriteLine($"Result: {res}");
+
+string GeneratePattern(int length)
+{
+    if (length % 2 != 0 || length < 2) return "Length Must Be Even Number and Greater than 0!";
+    StringBuilder builder = new StringBuilder();
+    int maxParentheses = length / 2;
+    List<int> parenthesesLayers = new List<int>();
+    Random random = new Random();
+
+    parenthesesLayers.Add(0);
+    builder.Append("()");
+
+    for (int i = 1; i < maxParentheses; i++)
+    {
+        int layer = random.Next(0, parenthesesLayers.Max() + 2);
+        parenthesesLayers.Add(layer);
+    }
+
+    parenthesesLayers = parenthesesLayers.Order().ToList();
+
+    for (int i = 0; i < parenthesesLayers.Count; i++)
+    {
+        Console.WriteLine($"i: {i}, Layer: {parenthesesLayers[i]}");
+    }
+
+    for (int i = 1; i < parenthesesLayers.Count; i++)
+    {
+        var possibleIndexes = FindPossibleIndexes(builder.ToString(), parenthesesLayers[i]);
+        int index = possibleIndexes[random.Next(0, possibleIndexes.Count)];
+        builder.Insert(index, "()");
+    }
+
+    return builder.ToString();
+}
+
+List<int> FindPossibleIndexes(string parentheses, int layer)
+{
+    List<int> indexes = new();
+    int currentLayer = 0;
+
+    for (int i = 0; i < parentheses.Length; i++)
+    {
+        if (parentheses[i] == '(')
+            currentLayer++;
+        else
+            currentLayer--;
+
+        if (currentLayer == layer) indexes.Add(i + 1);
+    }
+
+    // foreach (var index in indexes)
+    // {
+    //     Console.WriteLine($"Possible Index: {index}");
+    // }
+    // Console.WriteLine("");
+    return indexes;
+}
